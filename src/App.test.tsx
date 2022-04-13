b13b90ea0aa3ec245e7790ce300ad405a4e60e4c
jm <biaggi>
2022-04-13 12:35:33 +0200

    chore(): just to show a problem

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,18 @@
+import App from './App';
+/*
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App />);
+*/
+
+/*
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
@@ -13,3 +28,26 @@ test('renders learn react link', () => {
 
   expect(getByText(/learn/i)).toBeInTheDocument();
 });
+*/
+//import { createRoot } from "react-dom/client";
+/*
+import * as ReactDOMClient from 'react-dom/client';
+
+import App from "./App";
+
+test("renders learn react link", () => {
+
+  const rootElement = document.createElement("div");
+  console.log(rootElement);
+  rootElement.setAttribute("id", "root");
+  console.log(rootElement);
+  document.getElementsByTagName("body")[0].appendChild(rootElement);
+
+  const container = document.getElementById("root");
+  const aaa = ReactDOMClient.createRoot(container).render(<App />);
+  console.log(aaa, container)
+  const { getByText } = aaa
+
+  expect(getByText(/learn/i)).toBeInTheDocument();
+});
+*/
