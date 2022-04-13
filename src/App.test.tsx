94ef0f62837089134080dd47c6bace347d107dc3
jm <biaggi>
2022-04-13 14:12:40 +0200

    chore(): fixing template to match react 18 version

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,53 +1,38 @@
-import App from './App';
-/*
-import * as ReactDOMClient from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById('app');
-
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the root.
-root.render(<App />);
-*/
-
-/*
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
-*/
-//import { createRoot } from "react-dom/client";
-/*
-import * as ReactDOMClient from 'react-dom/client';
-
+import ReactDOM from "react-dom/client";
+import { act } from "react-dom/test-utils";
+import { store } from "./app/store";
 import App from "./App";
+import { Provider } from "react-redux";
+import { screen } from "@testing-library/dom";
 
-test("renders learn react link", () => {
-
-  const rootElement = document.createElement("div");
-  console.log(rootElement);
-  rootElement.setAttribute("id", "root");
-  console.log(rootElement);
-  document.getElementsByTagName("body")[0].appendChild(rootElement);
-
-  const container = document.getElementById("root");
-  const aaa = ReactDOMClient.createRoot(container).render(<App />);
-  console.log(aaa, container)
-  const { getByText } = aaa
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+// let container: Element | null = null;
+let root: Element | null = null;
+let reactDomRoot: ReactDOM.Root | null = null;
+describe("App", () => {
+  beforeEach(() => {
+    // setup a DOM element as a render target
+    root = document.createElement("div");
+    document.body.appendChild(root);
+  });
+  afterEach(() => {
+    // cleanup on exiting
+    act(() => {
+      if (reactDomRoot) reactDomRoot.unmount();
+    });
+    if (root) root.remove();
+    root = null;
+  });
+  test("renders learn react link", () => {
+    act(() => {
+      if (root) {
+        reactDomRoot = ReactDOM.createRoot(root);
+        reactDomRoot.render(
+          <Provider store={store}>
+            <App />
+          </Provider>
+        );
+      }
+    });
+    expect(screen.getByText("Learn")).toBeDefined();
+  });
 });
-*/
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import {createRoot} from 'react-dom/client';
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  
-);
-
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { createRoot } from "react-dom/client";
+const root = document.getElementById("root");
+if (root) {
+  createRoot(root).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
