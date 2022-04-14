b2ab9d8c3b68578876ee4b1bc51406bdfec8d51c
jm <biaggi>
2022-04-14 15:27:45 +0200

    chore(): cleaning up counter reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import githubReducer from '../features/github/githubSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     github: githubReducer
   },
 });
