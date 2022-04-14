cd5401bcfaf50c06eb88dec15c242de5c07f8580
jm <biaggi>
2022-04-14 14:31:04 +0200

    feat(router) added user page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Home } from './features/github/pages/Home';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Home } from "./features/github/pages/Home";
+import * as ReactDom from "react-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/"><img src={logo} className="App-logo" alt="logo" /></a>
+        <a href="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
         <Home />
       </header>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
+import { User } from "./features/github/pages/user/User";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { createRoot } from "react-dom/client";
+
+import * as ReactDom from "react-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 const root = document.getElementById("root");
 if (root) {
   createRoot(root).render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/user" element={<User />} />
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </React.StrictMode>
   );
